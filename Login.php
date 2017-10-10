2ce20a327650254f641bc1fda510b6f90483339d
narjiskhalid <32687180+narjiskhalid@users.noreply.github.com>
2017-10-10 23:52:47 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("localhost", "id3215533_userregister", "12345Narjis", "id3215533_userregister");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
